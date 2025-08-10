ba2dc590f8e81619b8dbc26cab089a2a08e4b1af
Jugal Bhatt <jugalbhatt3@gmail.com>
2025-08-10 12:14:00 -0700

    frotnend layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,10 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="min-h-screen bg-gray-50">
-          {children}
-        </div>
+      <body className={`${inter.className} antialiased`}>
+        <Providers>
+          <div className="min-h-screen bg-gray-50">
+            {children}
+          </div>
+        </Providers>
       </body>
     </html>
   )
